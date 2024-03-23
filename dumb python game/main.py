92235314632283d9f8d0eb94a18a08147d841edf
picola <157052926+picola18@users.noreply.github.com>
2024-03-23 19:15:11 -0300

    updated main.py

--- dumb python game/main.py
+++ dumb python game/main.py
@@ -1,32 +1,30 @@
 import pygame
 from sys import exit
 
-# starts pygame
-pygame.init()
-# starts the screen
-screen = pygame.display.set_mode((800, 400))
-# sets the game title
-pygame.display.set_caption("Placeholder Title")
-# sets the framerate
-clock = pygame.time.Clock()
-# creates a font
-test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+def display_score():
+    current_time = pygame.time.get_ticks() - start_time
+    score_surf = test_font.render(f'{current_time}',False,(64,64,64))
+    score_rect = score_surf.get_rect(center = (400, 50))
+    screen.blit(score_surf,score_rect)
 
-# creates a surface/image to put on screen, on this case the sky. the .convert just converts the png file into something pygame
-#  the png file into something pygame can utilize more easily.
+
+pygame.init()
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption("Placeholder Title")
+clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+game_active = True
+start_time = 0
+
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
-# same thing, but with the ground
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-text_surface = test_font.render('My game', False, 'black')
 
 # calling the enemy, convert_alpha respects the alpha values, so it doesn't do any weird shit.
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-# moves the snail x position to a variable
-snail_x_pos = 600
-# creating the hooman
+snail_rect = snail_surface.get_rect(bottomright = (600, 300))
 player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
-# creates the rectangle for the player, really easier.
 player_rect = player_surf.get_rect(midbottom = (80, 300))
+player_gravity = 0
 
 # creates the loop for the game
 while True:
@@ -35,25 +33,51 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    
+
+        if game_active == True:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    if player_rect.bottom >= 300:
+                        player_gravity = -20
+        
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if player_rect.collidepoint(event.pos):
+                    if player_rect.bottom >= 300:
+                        player_gravity = -20
+        else:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    game_active = True
+                    snail_rect.left = 800
+                    start_time = pygame.time.get_ticks()
+
+    if game_active:
     # blit essentially means block image transfer, which is a fancy way of saying put image on top of image.
-    screen.blit(sky_surface,(0,0))
-    # pygame always draws in order of when we run the code, so calling the ground after the sky is necessary.
-    screen.blit(ground_surface,(0,300))
-    screen.blit(text_surface,(325,50))
-    # moves the snail x position by one by updating its variable
-    snail_x_pos -= 4 
-    # makes an if statement that makes the snail go back to the end of the screen    
-    if snail_x_pos < -100:
-        snail_x_pos = 800
-    # puts the snail on screen, using variables as position
-    screen.blit(snail_surface,(snail_x_pos,265))
-    # makes the player go boom
-    player_rect.left += 1
-    # puts the player on screen.
-    screen.blit(player_surf,player_rect)
-   
-    # this will update every frame
+        screen.blit(sky_surface,(0,0))
+        screen.blit(ground_surface,(0,300))
+
+        # SNAIL
+        display_score()
+        snail_rect.x -= 4
+        if snail_rect.right <= 0:
+            snail_rect.left = 800
+        screen.blit(snail_surface,(snail_rect.x,265))
+        #SNAIL
+
+        # PLAYER
+        player_gravity += 1
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 300:
+            player_rect.bottom = 300
+        screen.blit(player_surf,player_rect)
+        # PLAYER
+
+        # COLLISIONS    
+        if snail_rect.colliderect(player_rect):
+            game_active = False
+        # COLLISIONS
+    else:
+        screen.fill('Yellow')
+
     pygame.display.update()
-    # sets le frame rate
     clock.tick(60)
